c065da77c622f0b196b4c266a233612c50f900a6
casey bradford <casey@flatironschool.com>
2018-05-13 20:57:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // write your code below!
-var holiday = "Independence Day"
 var days = 20
 function happyHolidays() {
   return "Happy holidays!"
 }
 
 function happyHolidaysTo (name) {
-  return `Happy holidays ${name}!`
+  return `Happy holidays, ${name}!`
 }
 
 function happyHolidayTo(holiday, name){
+  var holiday = "Independence Day"
   return `Happy ${holiday}, ${name}!`
 }
 
 function holidayCountdown (days, holiday) {
-  return `It's ${days} days untill ${holiday}!`
+  return `It's ${days} days until ${holiday}!`
 }
