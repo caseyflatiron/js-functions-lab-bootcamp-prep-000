31c44e82f009c6acb8155f2d02966af562bb450b
casey bradford <casey@flatironschool.com>
2018-05-13 20:54:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo (name) {
-  return 'Happy holidays ${name}!'
+  return `Happy holidays ${name}!`
 }
 
 function happyHolidayTo(holiday, name){
-  return 'Happy ${holiday}, ${name}!'
+  return `Happy ${holiday}, ${name}!`
 }
 
 function holidayCountdown (days, holiday) {
-  return 'It\'s ${days} days untill ${holiday}!'
+  return `It's ${days} days untill ${holiday}!`
 }
